cf3fb903e43f43a617212a7f7b9e0aefd9e10ae0
Matt McKegg <matt@wetsand.co.nz>
2016-10-23 22:31:34 +1300

    fix asynchronous usage

--- index.js
+++ index.js
@@ -11,30 +11,33 @@ function catMap (fn) {
       }
 
       function more () {
-        if (!stream) {
+        if (stream) {
+          readStream()
+        } else {
           read(null, function (end, data) {
             if (end) return cb(end)
             var mapped = fn(data)
             if (typeof mapped === 'function') {
               stream = mapped
+              readStream()
             } else {
               cb(null, mapped)
             }
           })
         }
+      }
 
-        if (stream) {
-          stream(null, function (endOrErr, data) {
-            if (endOrErr === true) {
-              stream = null
-              more()
-            } else if (endOrErr) {
-              cb(endOrErr)
-            } else {
-              cb(null, data)
-            }
-          })
-        }
+      function readStream () {
+        stream(null, function (endOrErr, data) {
+          if (endOrErr === true) {
+            stream = null
+            more()
+          } else if (endOrErr) {
+            cb(endOrErr)
+          } else {
+            cb(null, data)
+          }
+        })
       }
     }
   }
